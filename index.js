e6781b52757511d9106dc840d8db4660a2076222
Andi <andi@noname-media.com>
2014-06-08 10:01:12 +0200

    Modify index.js

--- index.js
+++ index.js
@@ -4,33 +4,59 @@ module.exports = function(coffeeBreak) {
 	var fs = require('fs'),
 		path = require('path');
 
-	var istanbul = require('istanbul'),
+	var Istanbul = require('istanbul'),
 		mkdirp = require('mkdirp');
 
 	coffeeBreak.registerTask('codecoverage', function(conf, logger, done) {
-		console.log('Run code coverage', conf);
+		// console.log('Run code coverage', conf);
 
 		var files = conf.files,
 			tmpDir = path.join(__dirname, 'instrumented', conf.project);
 
-		console.log('Code coverage out dir:', tmpDir);
+		// console.log('Code coverage out dir:', tmpDir);
 
 		files.map(function(file) {
-			console.log('File:', file);
+			// console.log('File:', file);
 			var source = fs.readFileSync(conf.cwd + '/' + file, 'utf8');
-			var instrumenter = new istanbul.Instrumenter(),
+			var instrumenter = new Istanbul.Instrumenter(),
 				instrumented = instrumenter.instrumentSync(source, file);
 
 			mkdirp.sync(path.dirname(tmpDir + '/' + file));
 			fs.writeFileSync(tmpDir + '/' + file, instrumented);
 
-			console.log('Write instrumented file:', tmpDir + '/' + file);
+			// console.log('Write instrumented file:', tmpDir + '/' + file);
 		});
 
 		conf.cwdInstrumented = tmpDir;
 
-		console.log('Conf after instrumentation', conf);
+		// console.log('Conf after instrumentation', conf);
 
 		done();
 	});
+
+	var generateReport = function(conf) {
+		var result = {"lib/lib1.js":{"path":"lib/lib1.js","s":{"1":1,"2":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":2,"loc":{"start":{"line":2,"column":7},"end":{"line":2,"column":18}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":5,"column":2}},"2":{"start":{"line":3,"column":2},"end":{"line":3,"column":17}}},"branchMap":{}},"lib/lib2.js":{"path":"lib/lib2.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}},"lib/lib3.js":{"path":"lib/lib3.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}},"modulea.js":{"path":"modulea.js","s":{"1":1},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":6}}},"branchMap":{}},"superModule.js":{"path":"superModule.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}}};
+
+		var Store = Istanbul.Store,
+			store = Store.create('memory');
+
+		for (var key in result) {
+			var source = fs.readFileSync(conf.cwd + '/' + key, 'utf8');
+			// console.log('Addsource of:', key, source);
+			store.set(key, source);
+		}
+
+		store.dispose();
+
+		var Report = require('istanbul').Report,
+			report = Report.create('html'),
+			collector = new Istanbul.Collector();
+
+		collector.add(result);
+
+		// console.log('Write result');
+		report.writeReport(collector,{
+			sourceStore: store
+		});
+	};
 };
